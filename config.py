fbc03f215e7d662161632f5868ffc327f3982cd9
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-19 10:28:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,10 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+MONGO_URL = getenv("MONGO_URL", "")
+SESSION_STRING = getenv("SESSION_STRING", "")
+
+
